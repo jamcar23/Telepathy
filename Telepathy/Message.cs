bd3757beed18f3b39aea312be0e89f06ad67886c
James Carroll <jamcar23@gmail.com>
2018-09-16 01:24:11 -0400

    Added alloc free
    * Added ByteArrayPool to reduce allocations.
    
    * restored tests and API.
    
    * obsolete attribute

--- Telepathy/Message.cs
+++ Telepathy/Message.cs
@@ -1,17 +1,36 @@
 ﻿// incoming message queue of <connectionId, message>
 // (not a HashSet because one connection can have multiple new messages)
+using System;
+
 namespace Telepathy
 {
-    public struct Message
+    public struct Message : IDisposable
     {
         public int connectionId;
         public EventType eventType;
-        public byte[] data;
-        public Message(int connectionId, EventType eventType, byte[] data)
+        public byte[] buffer;
+        public ArraySegment<byte> segment;
+        [Obsolete("Use segment instead, and Dispose messages.")]
+        public byte[] data {
+            get {
+                var array = new byte[segment.Count - segment.Offset];
+                Array.Copy(segment.Array, segment.Offset, array, 0, segment.Count);
+                return array;
+            }
+        }
+
+        public Message(int connectionId, EventType eventType, byte[] buffer, int size=0)
         {
             this.connectionId = connectionId;
             this.eventType = eventType;
-            this.data = data;
+            this.buffer = buffer;
+            this.segment = new ArraySegment<byte>(this.buffer, 0, size);
+        }
+
+        public void Dispose() 
+        {
+            if(buffer != null)
+                ByteArrayPool.Return(buffer);
         }
     }
 }
